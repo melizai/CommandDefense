79c94442c3cbbdbdff995073efb0713b8e0226d0
Iacob Eliza-Maria <100303019+melizai@users.noreply.github.com>
2022-03-16 20:20:56 +0200

    Add files via upload

---
+++ MyLibrary.cpp
@@ -0,0 +1,23 @@
+#include "MyLibrary.h"
+#include <cstdio>
+#include <cstdlib>     /* srand, rand */
+#include <ctime>       /* time */
+#include <fstream>
+
+void MyLibrary::print_books()
+{
+    output_stream << "Found " << books_number << " books" << endl;
+    for (int i = 0; i < books_number; i++)
+        output_stream << "Book: " << i << " " << books[i] << endl;
+}
+
+MyLibrary::~MyLibrary()
+{
+    delete[] books;
+}
+
+void MyLibrary::update_books_id_by_index(int book_index, int book_id)
+{
+    if (book_index < books_number)
+        this->books[book_index] = book_id;
+}
